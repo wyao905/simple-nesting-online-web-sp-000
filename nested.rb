08c2085de9ee5407c54feb04f4364f3789f7350e
Warren Yao <warrenyca905@gmail.com>
2019-05-23 02:23:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -36,7 +34,8 @@ def alan_kay_is_known_for
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+    }
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
