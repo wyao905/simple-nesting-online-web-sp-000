997000f4c9346a92a2589db9177bafa81ca0296a
Warren Yao <warrenyca905@gmail.com>
2019-05-23 02:28:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,20 +66,26 @@ def adding_matz
 
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+    }
+  }
+  
+  return programmer_hash
     
 end
 
